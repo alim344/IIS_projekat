bda2a58eb99ad2c59ed9e49e09ff767bf06f4e0f
alim344 <milabudimirovic2@gmail.com>
2026-01-12 17:34:44 +0100

    initial commit

---
+++ autoskola/src/main/java/com/example/autoskola/AutoskolaApplication.java
@@ -0,0 +1,13 @@
+package com.example.autoskola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoskolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoskolaApplication.class, args);
+	}
+
+}
